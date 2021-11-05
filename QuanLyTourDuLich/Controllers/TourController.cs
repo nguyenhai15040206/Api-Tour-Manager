952130ef526ca863a89959aad93d9c8caf837c5e
nguyenhai15040206 <nguyenhai15040206@gmail.com>
2021-11-05 21:06:31 +0700

    [tanhai-20211105][v1]: Xử lý tin tức

--- QuanLyTourDuLich/Controllers/TourController.cs
+++ QuanLyTourDuLich/Controllers/TourController.cs
@@ -20,97 +20,94 @@ namespace QuanLyTourDuLich.Controllers
             _context = context;
         }
 
-        // Lấy những tour nào được đề xuất max 9 tour
+        // Lấy những tour nào được đề xuất max 6 tour
         [HttpGet("TourIsSuggest")]
         public async Task<IActionResult> TourIsSuggest()
         {
-            var tourList = await (from t in _context.Tour
-                                  join p in _context.Province on t.DeparturePlace equals p.ProvinceId
-                                  join up in _context.UnitPrice on t.TourId equals up.TourId
-                                  join tg in _context.TourGuide on t.TourGuideId equals tg.TourGuideId into ttg
-                                  from a in ttg.DefaultIfEmpty()
-                                  where t.Suggest == true && (t.IsDelete == null || t.IsDelete == true)
-                                  orderby t.DateStart descending
-                                    select new
-                                    {
-                                        t.TourId,
-                                        t.TourName,
-                                        t.TourImg,
-                                        t.DateStart,
-                                        t.Rating,
-                                        up.AdultUnitPrice,
-                                        p.ProvinceName,
-                                        //tourGuideName = a.TourGuideName?? null,
-                                    }).Take(6).ToListAsync();
-            if(tourList == null || tourList.Count == 0)
-            {
-                return NoContent();
-            }
-
-            return Ok(tourList);
-        }
-
-
-        //Phân trang
-        public async Task<IActionResult> TourPaginationList(int page = 1, int limit = 9)
-        {
-            //List<Tour> tourListModels = new List<Tour>();
-            var tourList = await (from t in _context.Tour
-                                  join p in _context.Province on t.DeparturePlace equals p.ProvinceId
-                                  join up in _context.UnitPrice on t.TourId equals up.TourId
-                                  join tg in _context.TourGuide on t.TourGuideId equals tg.TourGuideId into ttg
-                                  from a in ttg.DefaultIfEmpty()
-                                  where t.Suggest == true && (t.IsDelete == null || t.IsDelete == true)
-                                  orderby t.DateStart descending
-                                  select new {
-                                      t.TourId,
-                                      t.TourName,
-                                      t.TourImg,
-                                      t.DateStart,
-                                      t.Rating,
-                                      up.AdultUnitPrice,
-                                      p.ProvinceName,
-                                  } ) .Skip((page - 1) * limit).Take(limit).ToListAsync();
-            var tourListModels = tourList;
-            int totalRecord = tourList.Count();
-            var pagination = new Pagination
-            {
-                count = totalRecord,
-                currentPage = page,
-                pagsize = limit,
-                totalPage = (int)Math.Ceiling(decimal.Divide(totalRecord, limit)),
-                indexOne = ((page - 1) * limit + 1),
-                indexTwo = (((page - 1) * limit + limit) <= totalRecord ? ((page - 1) * limit * limit) : totalRecord)
-            };
-            return new ObjectResult(new
-            {
-                data = tourListModels,
-                pagination = pagination
-            });
-        }
-
-        //get danh sách tour
-        [HttpGet("TourPaginationList")]
-        public async Task<IActionResult> GET(int page, int limit)
-        {
             try
             {
-                var rs = await TourPaginationList(page, limit);
-                if (rs == null)
+                var tourList = await (from t in _context.Tour
+                                      join p in _context.Province on t.DeparturePlace equals p.ProvinceId
+                                      join up in _context.UnitPrice on t.TourId equals up.TourId
+                                      join tg in _context.TourGuide on t.TourGuideId equals tg.TourGuideId into ttg
+                                      from a in ttg.DefaultIfEmpty()
+                                      where t.Suggest == true && (t.IsDelete == null || t.IsDelete == true)
+                                      orderby t.DateStart descending
+                                      select new
+                                      {
+                                          t.TourId,
+                                          t.TourName,
+                                          t.TourImg,
+                                          t.DateStart,
+                                          t.Rating,
+                                          up.AdultUnitPrice,
+                                          p.ProvinceName,
+                                          //tourGuideName = a.TourGuideName?? null,
+                                      }).Take(6).ToListAsync();
+                if (tourList == null || tourList.Count == 0)
                 {
                     return NotFound();
                 }
-                return new ObjectResult(rs);
+
+                return Ok(tourList);
             }
-            catch
+            catch (Exception)
             {
-                return BadRequest();
+                return StatusCode(StatusCodes.Status500InternalServerError, "Error retrieving data from the database");
             }
         }
 
 
+        // Lấy tất tả danh sách tour phân trang cho Client
+        [HttpGet("TourPaginationList")]
+        public async Task<IActionResult> TourPaginationList(int page = 1, int limit = 9)
+        {
+            //List<Tour> tourListModels = new List<Tour>();
+            try
+            {
+                var tourList = await (from t in _context.Tour
+                                      join p in _context.Province on t.DeparturePlace equals p.ProvinceId
+                                      join up in _context.UnitPrice on t.TourId equals up.TourId
+                                      join tg in _context.TourGuide on t.TourGuideId equals tg.TourGuideId into ttg
+                                      from a in ttg.DefaultIfEmpty()
+                                      where (t.IsDelete == null || t.IsDelete == true)
+                                      orderby t.DateStart descending
+                                      select new
+                                      {
+                                          t.TourId,
+                                          t.TourName,
+                                          t.TourImg,
+                                          t.DateStart,
+                                          t.Rating,
+                                          up.AdultUnitPrice,
+                                          p.ProvinceName,
+                                      }).Skip((page - 1) * limit).Take(limit).ToListAsync();
+                var tourListModels = tourList;
+                int totalRecord = tourList.Count();
+                var pagination = new Pagination
+                {
+                    count = totalRecord,
+                    currentPage = page,
+                    pagsize = limit,
+                    totalPage = (int)Math.Ceiling(decimal.Divide(totalRecord, limit)),
+                    indexOne = ((page - 1) * limit + 1),
+                    indexTwo = (((page - 1) * limit + limit) <= totalRecord ? ((page - 1) * limit * limit) : totalRecord)
+                };
+                return Ok(new
+                {
+                    data = tourListModels,
+                    pagination = pagination
+                });
+            }
+            catch (Exception)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, "Error retrieving data from the database");
+            }
+            
+        }
+
         // [Nguyễn Tấn Hải -] - Xử lý Lấy dữ liệu tourDetails
-        [HttpGet("TourDetails/{tourID}")]
+        [HttpGet("TourDetails/{tourID:int}")]
         public async Task<ActionResult> TourDetails(int tourID)
         {
             try
@@ -157,30 +154,33 @@ namespace QuanLyTourDuLich.Controllers
                 }
                 return Ok(tourDetails);
             }
-            catch
+            catch (Exception)
             {
-                return BadRequest();
+                return StatusCode(StatusCodes.Status500InternalServerError, "Error retrieving data from the database");
             }
         }
+        
+
+        // [Nguyễn Tấn Hải - 20211105]: Thực hiện Post Data
         [HttpPost]
-        public async Task<ActionResult<IEnumerable<Tour>>> InsertTour([FromBody] Tour tour)
+        public async Task<ActionResult<IEnumerable<Tour>>> POST([FromBody] Tour tour)
         {
             try
             {
-                if (tour != null)
+                if(tour==null)
                 {
-                    await _context.Tour.AddAsync(tour);
-                    await _context.SaveChangesAsync();
-                    return CreatedAtAction("GET", new { maTour = tour.TourId }, tour);
+                    // 400
+                    return BadRequest();
                 }
-                else
-                {
-                    return NoContent();
-                }
+                await _context.Tour.AddAsync(tour);
+                await _context.SaveChangesAsync();
+                // nếu oke => stattus code 201
+                return CreatedAtAction("GET", new { maTour = tour.TourId }, tour);
             }
-            catch
+            catch (Exception)
             {
-                return BadRequest();
+                // 500
+                return StatusCode(StatusCodes.Status500InternalServerError, "Error retrieving data from the database");
             }
         }
     }
