5bfd6a7b7cd69ccdab58d686bbf06038320e5140
nguyenhai15040206 <nguyenhai15040206@gmail.com>
2021-11-28 00:54:52 +0700

    Cập nhật

--- QuanLyTourDuLich/Controllers/ProvinceController.cs
+++ QuanLyTourDuLich/Controllers/ProvinceController.cs
@@ -38,5 +38,52 @@ namespace QuanLyTourDuLich.Controllers
                 return StatusCode(StatusCodes.Status500InternalServerError, "Error retrieving data from the database");
             }
         }
+
+        [HttpPost("Adm_GetProvinceAndSearch")]
+        public async Task<IActionResult> Adm_GetProvinceAndSearch([FromBody] ProvinceSearchModel provinceSearch)
+        {
+            try
+            {
+                bool checkModelSearchIsNull = true;
+
+                bool isProvinceID = false;
+                if (provinceSearch.provinceID.Length > 0)
+                {
+                    isProvinceID = true;
+                }
+                //bool isProvinceID = int.TryParse(provinceSearch.provinceID.ToString(), out int provinceID);
+                bool isProvinceName = (!string.IsNullOrEmpty(provinceSearch.provinceName));
+                bool isDivisionType = (!string.IsNullOrEmpty(provinceSearch.divisionType));
+
+                if (isDivisionType || isProvinceID || isProvinceName)
+                {
+                    checkModelSearchIsNull = false;
+                }
+                var rs = await (from p in _context.Province
+                                where checkModelSearchIsNull == true ? true
+                                : isProvinceID == true ? (provinceSearch.provinceID.Contains(p.ProvinceId))
+                                : (
+                                    (isProvinceName && p.ProvinceName.Contains(provinceSearch.provinceName))
+                                    || (isDivisionType && p.DivisionType.Contains(provinceSearch.divisionType))
+                                )
+                                select new
+                                {
+                                    value = p.ProvinceId,
+                                    label = p.ProvinceName,
+                                    p.DivisionType,
+                                    p.Regions,
+                                    count = _context.TouristAttraction.Where(m => m.ProvinceId == p.ProvinceId).Count()
+                                }).ToListAsync();
+                if (rs == null)
+                {
+                    return BadRequest();
+                }
+                return Ok(rs);
+            }
+            catch
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, "Error retrieving data from the database");
+            }
+        }
     }
 }
